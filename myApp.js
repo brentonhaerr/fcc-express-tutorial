3102d83f4455ae30efd7d58c85f4aaea4125363b
Brenton Haerr <brentonhaerr@Brentons-MacBook-Air.local>
2022-06-19 09:14:17 -0700

    realized heroku does not use .env

--- myApp.js
+++ myApp.js
@@ -2,12 +2,14 @@ let express = require('express');
 let app = express();
 require('dotenv').config();
 
+// NOTE: Heroku does not use .env files. Must be set in the Heroku app.
+
 function serveJSON(req, res) {
   let message = "Hello json";
   if (process.env.MESSAGE_STYLE === 'uppercase') {
     message = message.toUpperCase();
   }
-  res.json({"message":message, "message_style": process.env.MESSAGE_STYLE});
+  res.json({"message":message});
 }
 
 app.get("/json", serveJSON);
