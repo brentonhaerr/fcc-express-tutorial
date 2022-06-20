161c116306ae92d6b18e40dc39cee5228afae15f
Brenton Haerr <brentonhaerr@Brentons-MacBook-Air.local>
2022-06-20 16:33:16 -0700

    Lesson 10 solved.

--- myApp.js
+++ myApp.js
@@ -1,21 +1,21 @@
 let express = require('express');
 let app = express();
 
-// Get Route Parameter Input from the Client
+// Get Query Parameter Input from the Client
 
-// When building an API, we have to allow users to communicate to us what they want to get from our service. For example, if the client is requesting information about a user stored in the database, they need a way to let us know which user they're interested in. One possible way to achieve this result is by using route parameters. Route parameters are named segments of the URL, delimited by slashes (/). Each segment captures the value of the part of the URL which matches its position. The captured values can be found in the req.params object.
+// Another common way to get input from the client is by encoding the data after the route path, using a query string. The query string is delimited by a question mark (?), and includes field=value couples. Each couple is separated by an ampersand (&). Express can parse the data from the query string, and populate the object req.query. Some characters, like the percent (%), cannot be in URLs and have to be encoded in a different format before you can send them. If you use the API from JavaScript, you can use specific methods to encode/decode these characters.
 
-// route_path: '/user/:userId/book/:bookId'
-// actual_request_URL: '/user/546/book/6754'
-// req.params: {userId: '546', bookId: '6754'}
+// route_path: '/library'
+// actual_request_URL: '/library?userId=546&bookId=6754'
+// req.query: {userId: '546', bookId: '6754'}
+// Build an API endpoint, mounted at GET /name. Respond with a JSON document, taking the structure { name: 'firstname lastname'}. The first and last name parameters should be encoded in a query string e.g. ?first=firstname&last=lastname.
 
-// Build an echo server, mounted at the route GET /:word/echo. Respond with a JSON object, taking the structure {echo: word}. You can find the word to be repeated at req.params.word. You can test your route from your browser's address bar, visiting some matching routes, e.g. your-app-rootpath/freecodecamp/echo.
+// Note: In the following exercise you are going to receive data from a POST request, at the same /name route path. If you want, you can use the method app.route(path).get(handler).post(handler). This syntax allows you to chain different verb handlers on the same path route. You can save a bit of typing, and have cleaner code.
 
-app.get("/:word/echo", function(req, res) {
-  res.json({ echo: req.params.word })
+app.get("/name", function(req, res) {
+  let name = req.query.first + " " + req.query.last;
+  res.json({ name });
 });
-
-
 
 
 
